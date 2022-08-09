dc93550b683ef2eeea1b7d7f5d493da282cbbe26
Ivan <adamovskij00@mail.ru>
2022-08-09 18:08:01 +0300

    add upd TodoItem

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { TodoItemButton, TodoItemCheckbox, TodoItemText, TodoItemWrapper } from './styled'
+import { TodoItemButton, TodoItemCheckbox, TodoItemWrapper } from './styled'
+import EditableField from '../EditableField'
 
 class TodoItem extends Component {
 
@@ -7,26 +8,37 @@ class TodoItem extends Component {
     this.props.completeTodo(this.props.todo.id)
   }
 
+  handleUpdate(newTitle) {
+    this.props.updateTodo(this.props.todo.id, newTitle)
+  }
+
   handleDelete() {
     this.props.deleteTodo(this.props.todo.id)
   }
 
   render() {
-    const { todo } = this.props
+    const { completed, text } = this.props.todo
 
     return (
       <TodoItemWrapper>
         <TodoItemCheckbox
           type='checkbox'
-          checked={todo.completed}
+          checked={completed}
           onChange={this.handleComplete.bind(this)}
         />
-        <TodoItemText>{todo.text}</TodoItemText>
-        <TodoItemButton onClick={this.handleDelete.bind(this)}>X</TodoItemButton>
+        <EditableField
+          title={text}
+          onChange={this.handleUpdate.bind(this)}
+        />
+        <TodoItemButton
+          onClick={this.handleDelete.bind(this)}
+        >
+          X
+        </TodoItemButton>
       </TodoItemWrapper>
     )
   }
 
 }
 
-export default TodoItem
+export default React.memo(TodoItem)
