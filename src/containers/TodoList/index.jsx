7ea1af39f0ffbd24a1aeedc3f11e2521e7f143fb
Ivan <adamovskij00@mail.ru>
2022-08-09 18:08:06 +0300

    add upd TodoList

--- src/containers/TodoList/index.jsx
+++ src/containers/TodoList/index.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import TodoItem from '../../components/TodoItem/index.jsx'
 import { TodoListEmpty, TodoListWrapper } from './styled'
 import { connect } from 'react-redux'
+import { filterTodos } from '../../helpers'
 
 class TodoList extends React.Component {
   render() {
-    const { todos, completeTodo, deleteTodo } = this.props
+    const { todos, completeTodo, deleteTodo, updateTodo } = this.props
 
     const todosList = todos.map((todo) => {
       return (
@@ -14,6 +15,7 @@ class TodoList extends React.Component {
           todo={todo}
           deleteTodo={deleteTodo}
           completeTodo={completeTodo}
+          updateTodo={updateTodo}
         />
       )
     })
@@ -21,21 +23,12 @@ class TodoList extends React.Component {
     return (
       <TodoListWrapper>
         {
-          todos.length === 0 ? <TodoListEmpty>Empty list</TodoListEmpty> : todosList
+          todos.length === 0 ?
+            <TodoListEmpty>Empty list</TodoListEmpty> :
+            todosList
         }
       </TodoListWrapper>
     )
-  }
-}
-
-const filterTodos = (todos, filter) => {
-  switch (filter) {
-    case 'SHOW_COMPLETE':
-      return todos.filter(todo => todo.completed === true)
-    case 'SHOW_ACTIVE':
-      return todos.filter(todo => todo.completed === false)
-    default:
-      return todos
   }
 }
 
