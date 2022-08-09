eaff24a97cf302f4d7083e8f8039813561092b12
Ivan <adamovskij00@mail.ru>
2022-08-09 18:07:45 +0300

    add upd Filter

--- src/components/Filters/index.jsx
+++ src/components/Filters/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Button from '../Button'
 import { FilterContainer } from './styled'
+import { typeFilter } from '../../constants'
 
 class Filter extends React.Component {
   render() {
     return (
       <FilterContainer>
-        <Button filter={'SHOW_ALL'}>All</Button>
-        <Button filter={'SHOW_ACTIVE'}>Active</Button>
-        <Button filter={'SHOW_COMPLETE'}>Complete</Button>
+        <Button filter={typeFilter.SHOW_ALL}>All</Button>
+        <Button filter={typeFilter.SHOW_ACTIVE}>Active</Button>
+        <Button filter={typeFilter.SHOW_COMPLETE}>Complete</Button>
       </FilterContainer>
     )
   }
 }
 
-export default Filter
+export default React.memo(Filter)
