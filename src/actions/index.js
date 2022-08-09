09adb8344967e95127009b53bc9074b7ee7a57a9
Ivan <adamovskij00@mail.ru>
2022-08-09 18:06:24 +0300

    add UPDATE_TODO action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
-import { ADD_TODO, COMPLETE_TODO, DELETE_TODO, FILTER_TODO } from '../constants/actions'
+import { ADD_TODO, COMPLETE_TODO, DELETE_TODO, FILTER_TODO, UPDATE_TODO } from '../constants/actions'
 
 export const addTodo = text => ({ type: ADD_TODO, payload: { text } })
 export const deleteTodo = id => ({ type: DELETE_TODO, payload: { id } })
+export const updateTodo = (id, text) => ({ type: UPDATE_TODO, payload: { id, text } })
 export const completeTodo = id => ({ type: COMPLETE_TODO, payload: { id } })
 export const filterTodo = filter => ({ type: FILTER_TODO, payload: { filter } })
