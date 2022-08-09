493cd5f0f0109df8cf972bdef7d2d872d7bac415
Ivan <adamovskij00@mail.ru>
2022-08-09 14:53:43 +0300

    add store

---
+++ src/store.js
@@ -0,0 +1,13 @@
+import { createStore } from 'redux'
+
+import { rootReducer } from './reducers'
+import { loadState, saveState } from './localStorage'
+
+const store = createStore(
+  rootReducer,
+  loadState(),
+)
+
+store.subscribe(() => saveState(store.getState()))
+
+export default store
